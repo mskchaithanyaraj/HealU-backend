967c02dd91a78177e87f38ef68436741855c0023
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-05-21 10:22:10 +0530

    added local origin-for cors

--- server.js
+++ server.js
@@ -10,9 +10,20 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// CORS options to allow requests from localhost:3000
+const allowedOrigins = [
+  "https://healuonline.netlify.app",
+  "http://localhost:3000",
+];
+
 const corsOptions = {
-  origin: ["https://healuonline.netlify.app", "http://localhost:3000"],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
   optionsSuccessStatus: 200,
 };
 
