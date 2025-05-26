fb9f8b26194db6f8d5f37280e77f6030e1fd87ee
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-05-26 09:31:48 +0530

    Added CORS for hosted domains

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "https://healuonline.netlify.app",
   "http://localhost:3000",
+  "http://healu.chaithanyaraj.live/",
+  "https://healu.chaithanyaraj.live/",
 ];
 
 const corsOptions = {
