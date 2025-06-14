459af9dfce0680b25829431f833b656cd7b06bba
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-06-14 10:26:58 +0530

    commented out localhost

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "https://healuonline.netlify.app",
-  "http://localhost:3000",
+  // "http://localhost:3000",
   "http://healu.chaithanyaraj.live",
   "https://healu.chaithanyaraj.live",
 ];
