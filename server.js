5cf597be409c4c6cd2414cabbd61f52a65235dac
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-06-14 10:26:15 +0530

    fixed url naming

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "https://healuonline.netlify.app",
   "http://localhost:3000",
-  "http://healu.chaithanyaraj.live/",
-  "https://healu.chaithanyaraj.live/",
+  "http://healu.chaithanyaraj.live",
+  "https://healu.chaithanyaraj.live",
 ];
 
 const corsOptions = {
